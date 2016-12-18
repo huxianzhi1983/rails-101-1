5327e3f1b0e138b29270df64c35f45ee8f21f88a
huxianzhi <hxz19830519@163.com>
2016-12-18 21:37:59 +0800

    render partial with collection

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :groups
+         has_many :posts
 
   def is_member_of?(group)
     participated_groups.include?(group)
